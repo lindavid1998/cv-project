8cab30b43c61d549663d49a09f50d5f9579f77fe
David Lin <lindavid1998@gmail.com>
2023-04-01 11:42:13 -0700

    Add title and basic form toggling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import Overview from './components/Overview';
 import Education from './components/Education';
 import Work from './components/Work';
 import Skills from './components/Skills';
+import Section from './components/Section';
 import './styles/App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<Overview></Overview>
+			<Section
+				title="Test"
+				formFields={{
+					name: '',
+					degree: '',
+					start: '',
+					end: '',
+					description: '',
+				}}
+			/>
 			<Education></Education>
 			<Work></Work>
 			<Skills></Skills>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
+import Form from './Form';
+import Button from './Button';
+import '../styles/Section.css';
 class Section extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			add: false,
+		};
+
+		this.handleClickAdd = this.handleClickAdd.bind(this);
+		this.handleClickCancel = this.handleClickCancel.bind(this);
+	}
+
+	handleClickAdd() {
+		this.setState({
+			add: true,
+		});
+	}
+
+	handleClickCancel() {
+		this.setState({
+			add: false,
+		});
+	}
+
 	render() {
-		return <div></div>
+		const { add } = this.state;
+		const { title, formFields } = this.props;
+
+		return (
+			<div className={`Section ${title}`}>
+				<div className="header">
+					<h1>{title}</h1>
+					<Button text="Add" onClick={this.handleClickAdd} className="add" />
+				</div>
+
+				{add && (
+					<Form fields={formFields} onClickCancel={this.handleClickCancel} />
+				)}
+			</div>
+		);
 	}
 }
 
