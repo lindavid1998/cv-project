2762e34c9ff0956db107750fbe84b3b776e9c6d8
David Lin <lindavid1998@gmail.com>
2023-04-08 13:28:19 -0700

    Convert Form.js and ImageForm.js to functional components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from './Button';
 import '../styles/Form.css';
 
-class Form extends Component {
-	render() {
-		const { fields, onSubmit, onClickCancel } = this.props;
-
-		return (
-			<form onSubmit={onSubmit}>
-				{Object.keys(fields).map((key, i) => {
-					if (key === 'id') return null;
-					return (
-						<div className='form-row' key={i}>
-							<label htmlFor={key}>
-								{key.charAt(0).toUpperCase() + key.slice(1)}
-							</label>
-							<input
-								type='text'
-								id={key}
-								name={key}
-								defaultValue={fields[key]}
-							></input>
-						</div>
-					);
-				})}
-				<div className='form-row'>
-					<input type='submit' value='Submit'></input>
-					<Button text='Cancel' onClick={onClickCancel} className='cancel' />
-				</div>
-			</form>
-		);
-	}
+function Form({ fields, onSubmit, onClickCancel }) {
+	return (
+		<form onSubmit={onSubmit}>
+			{Object.keys(fields).map((key, i) => {
+				if (key === 'id') return null;
+				return (
+					<div className='form-row' key={i}>
+						<label htmlFor={key}>
+							{key.charAt(0).toUpperCase() + key.slice(1)}
+						</label>
+						<input
+							type='text'
+							id={key}
+							name={key}
+							defaultValue={fields[key]}
+						></input>
+					</div>
+				);
+			})}
+			<div className='form-row'>
+				<input type='submit' value='Submit'></input>
+				<Button text='Cancel' onClick={onClickCancel} className='cancel' />
+			</div>
+		</form>
+	);
 }
 
 export default Form;
--- src/components/ImageForm.js
+++ src/components/ImageForm.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from './Button';
 
-class ImageForm extends Component {
-	render() {
-		const { onClickCancel, onSubmit } = this.props;
-
-		return (
-			<form onSubmit={onSubmit}>
-				<div className='form-row'>
-					<input
-						type='file'
-						id='avatar-img'
-						name='avatar-img'
-						accept='image/png, image/jpeg'
-					/>
-				</div>
-				<div className='form-row'>
-					<input type='submit' value='Submit'></input>
-					<Button text='Cancel' onClick={onClickCancel} className='cancel' />
-				</div>
-			</form>
-		);
-	}
+function ImageForm({ onClickCancel, onSubmit }) {
+	return (
+		<form onSubmit={onSubmit}>
+			<div className='form-row'>
+				<input
+					type='file'
+					id='avatar-img'
+					name='avatar-img'
+					accept='image/png, image/jpeg'
+				/>
+			</div>
+			<div className='form-row'>
+				<input type='submit' value='Submit'></input>
+				<Button text='Cancel' onClick={onClickCancel} className='cancel' />
+			</div>
+		</form>
+	);
 }
 
 export default ImageForm;
