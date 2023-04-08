0f64f387af46d013146ee75e748fa1ef3b624c0b
David Lin <lindavid1998@gmail.com>
2023-04-08 13:24:16 -0700

    Convert Section and Subsection to functional components

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,121 +1,96 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Form from './Form';
 import Button from './Button';
 import Subsection from './Subsection';
 import '../styles/Section.css';
-class Section extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [],
-			isFormVisible: false,
-			count: 0,
-			editID: null,
-		};
-		this.showForm = this.showForm.bind(this);
-		this.handleDelete = this.handleDelete.bind(this);
-		this.handleEdit = this.handleEdit.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
 
-	showForm(value) {
-		this.setState({
-			isFormVisible: value,
-		});
+export default function Section(props) {
+	const { title, formFields } = props;
+
+	const [data, setData] = useState([]);
+	const [isFormVisible, setIsFormVisible] = useState(false);
+	const [count, setCount] = useState(0);
+	const [editID, setEditID] = useState(null);
+
+	const showForm = (value) => {
+		setIsFormVisible(value);
 
 		if (!value) {
-			this.setState({
-				editID: null,
-			});
+			setEditID(null);
 		}
-	}
+	};
 
-	handleDelete(e) {
+	const handleDelete = (e) => {
 		const id = e.currentTarget.parentNode.parentNode.id;
-		this.setState({
-			data: this.state.data.filter((info) => info.id != id),
-		});
-	}
+		setData(data.filter((info) => info.id != id));
+	};
 
-	handleEdit(e) {
+	const handleEdit = (e) => {
 		const id = e.currentTarget.parentNode.parentNode.id;
-		this.setState({
-			isFormVisible: true,
-			editID: id,
-		});
-	}
+		setIsFormVisible(true);
+		setEditID(id);
+	};
 
-	handleSubmit(e) {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 
 		const formData = new FormData(e.target);
 
-		const keys = Object.keys(this.props.formFields);
+		const keys = Object.keys(formFields);
 
 		const newData = keys.reduce((acc, key) => {
 			acc[key] = formData.get(key);
 			return acc;
 		}, {});
 
-		newData['id'] = this.state.count;
+		newData['id'] = count;
 
-		let data;
-		if (this.state.editID) {
-			data = this.state.data;
-			let i = data.findIndex((info) => info.id == this.state.editID);
+		if (editID) {
+			let i = data.findIndex((info) => info.id == editID);
 			data[i] = Object.assign({}, data[i], newData);
 		}
 
-		this.setState({
-			data: this.state.editID ? data : this.state.data.concat(newData),
-			isFormVisible: false,
-			editID: null,
-			count: this.state.count + 1,
-		});
-	}
+		setData(editID ? data : data.concat(newData));
+		setIsFormVisible(false);
+		setEditID(null);
+		setCount(count + 1);
+	};
 
-	render() {
-		const { isFormVisible, data, editID } = this.state;
-		const { title, formFields } = this.props;
-
-		const subsections = data.map((info, i) => {
-			return (
-				<Subsection
-					info={info}
-					onClickDelete={this.handleDelete}
-					onClickEdit={this.handleEdit}
-					key={i}
-				/>
-			);
-		});
-
-		const formProps = {
-			fields: data.find((info) => info.id == editID) || formFields,
-			onClickCancel: () => this.showForm(false),
-			onSubmit: this.handleSubmit,
-		};
-
+	const subsections = data.map((info, i) => {
 		return (
-			<div className={`Section ${title}`}>
-				<div className='header'>
-					<h1>{title}</h1>
-					<Button
-						text='Add'
-						className='add'
-						onClick={() => this.showForm(true)}
-					/>
-				</div>
-
-				{title === 'Skills' ? (
-					<ul className='skills-list'>{subsections}</ul>
-				) : (
-					subsections
-				)}
-
-				{isFormVisible && <Form {...formProps} />}
-			</div>
+			<Subsection
+				info={info}
+				onClickDelete={handleDelete}
+				onClickEdit={handleEdit}
+				key={i}
+			/>
 		);
-	}
-}
+	});
 
-export default Section;
+	const formProps = {
+		fields: data.find((info) => info.id == editID) || formFields,
+		onClickCancel: () => showForm(false),
+		onSubmit: handleSubmit,
+	};
+
+	return (
+		<div className={`Section ${title}`}>
+			<div className='header'>
+				<h1>{title}</h1>
+				<Button
+					text='Add'
+					className='add'
+					onClick={() => showForm(true)}
+				/>
+			</div>
+
+			{title === 'Skills' ? (
+				<ul className='skills-list'>{subsections}</ul>
+			) : (
+				subsections
+			)}
+
+			{isFormVisible && <Form {...formProps} />}
+		</div>
+	);
+}
--- src/components/Subsection.js
+++ src/components/Subsection.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from './Button';
 import '../styles/Subsection.css';
 
-class Subsection extends Component {
-	render() {
-		const { info, onClickDelete, onClickEdit } = this.props;
+export default function Subsection(props) {
+	const { info, onClickDelete, onClickEdit } = props;
 
-		if (info.skill) {
-			return (
-				<li className="skill" key={info.id} id={info.id}>
-					<div>
-						<p className="skill-name">{info.skill}</p>
-						<Button text="Edit" className="edit" onClick={onClickEdit} />
-						<Button text="Delete" className="delete" onClick={onClickDelete} />
-					</div>
-				</li>
-			);
-		}
-
+	if (info.skill) {
 		return (
-			<div className="Subsection" id={info.id}>
-				<h2 className="header">{info.name || info.company}</h2>
-				<p className="subheader">{info.degree || info.title}</p>
-				<p className="duration">
-					{info.start} - {info.end}
-				</p>
-				{info.description && <p className="description">{info.description}</p>}
-				<div className="buttons">
-					<Button text="Edit" className="edit" onClick={onClickEdit} />
-					<Button text="Delete" className="delete" onClick={onClickDelete} />
+			<li className='skill' key={info.id} id={info.id}>
+				<div>
+					<p className='skill-name'>{info.skill}</p>
+					<Button text='Edit' className='edit' onClick={onClickEdit} />
+					<Button text='Delete' className='delete' onClick={onClickDelete} />
 				</div>
-			</div>
+			</li>
 		);
 	}
-}
 
-export default Subsection;
+	return (
+		<div className='Subsection' id={info.id}>
+			<h2 className='header'>{info.name || info.company}</h2>
+			<p className='subheader'>{info.degree || info.title}</p>
+			<p className='duration'>
+				{info.start} - {info.end}
+			</p>
+			{info.description && <p className='description'>{info.description}</p>}
+			<div className='buttons'>
+				<Button text='Edit' className='edit' onClick={onClickEdit} />
+				<Button text='Delete' className='delete' onClick={onClickDelete} />
+			</div>
+		</div>
+	);
+}
