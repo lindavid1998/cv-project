3380c0ec9dfdf4a784087f66083e4457fcad6862
David Lin <lindavid1998@gmail.com>
2023-04-08 13:26:08 -0700

    Convert Button.js to functional component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/Button.css';
 
-class Button extends Component {
-	render() {
-		const { text, onClick, className, id } = this.props;
-
-		let icon;
-		switch (text) {
-			case 'Add':
-				icon = <i className='fa-solid fa-circle-plus'></i>;
-				break;
-			case 'Edit':
-				icon = <i className='fa-solid fa-pen'></i>;
-				break;
-			case 'Delete':
-				icon = <i className='fa-regular fa-trash-can'></i>;
-				break;
-			case 'Cancel':
-				icon = text;
-				break;
-			default:
-				icon = null;
-		}
-
-		return (
-			<button onClick={onClick} id={id} className={className} type='button'>
-				{icon}
-			</button>
-		);
+function Button({ text, onClick, className, id }) {
+	let icon;
+	switch (text) {
+		case 'Add':
+			icon = <i className='fa-solid fa-circle-plus'></i>;
+			break;
+		case 'Edit':
+			icon = <i className='fa-solid fa-pen'></i>;
+			break;
+		case 'Delete':
+			icon = <i className='fa-regular fa-trash-can'></i>;
+			break;
+		case 'Cancel':
+			icon = text;
+			break;
+		default:
+			icon = null;
 	}
+
+	return (
+		<button onClick={onClick} id={id} className={className} type='button'>
+			{icon}
+		</button>
+	);
 }
 
 export default Button;
