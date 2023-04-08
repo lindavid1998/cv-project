6ce5065481a5eb744933124609238d1633133e92
David Lin <lindavid1998@gmail.com>
2023-04-08 13:10:11 -0700

    Convert Overview.js from class to functional component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,75 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import Form from './Form';
 import Avatar from './Avatar';
 import '../styles/Overview.css';
 
-class Overview extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			info: {
-				name: 'Your name',
-				title: 'Your title',
-				phone: 'xxx-xxx-xxxx',
-				email: 'xxx@email.com',
-			},
-			isFormVisible: false,
-		};
+export default function Overview() {
+	const [info, setInfo] = useState({
+		name: 'Your name',
+		title: 'Your title',
+		phone: 'xxx-xxx-xxxx',
+		email: 'xxx@email.com',
+	});
 
-		this.toggleForm = this.toggleForm.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+	const [isFormVisible, setIsFormVisible] = useState(false);
 
-	toggleForm() {
-		this.setState({
-			isFormVisible: !this.state.isFormVisible,
-		});
-	}
+	const toggleForm = () => {
+		setIsFormVisible(!isFormVisible);
+	};
 
-	handleSubmit(e) {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 
 		const formData = new FormData(e.target);
 
-		this.setState({
-			info: {
-				name: formData.get('name'),
-				title: formData.get('title'),
-				phone: formData.get('phone'),
-				email: formData.get('email'),
-			},
-			isFormVisible: false,
+		setInfo({
+			name: formData.get('name'),
+			title: formData.get('title'),
+			phone: formData.get('phone'),
+			email: formData.get('email'),
 		});
-	}
 
-	render() {
-		const { info, isFormVisible } = this.state;
-		const { name, title, phone, email } = info;
+		setIsFormVisible(false);
+	};
 
-		return (
-			<div className='Overview'>
-				<Avatar />
-				<h1 className='name'>{name}</h1>
-				<h2 className='title'>{title}</h2>
-				<div className='contact-info'>
-					<div>{phone}</div>
-					<div>|</div>
-					<div>{email}</div>
-				</div>
+	const { name, title, phone, email } = info
 
-				<Button text='Edit' onClick={this.toggleForm} className='edit' />
-
-				{isFormVisible && (
-					<Form
-						fields={info}
-						onSubmit={this.handleSubmit}
-						onClickCancel={this.toggleForm}
-					/>
-				)}
+	return (
+		<div className='Overview'>
+			<Avatar />
+			<h1 className='name'>{name}</h1>
+			<h2 className='title'>{title}</h2>
+			<div className='contact-info'>
+				<div>{phone}</div>
+				<div>|</div>
+				<div>{email}</div>
 			</div>
-		);
-	}
-}
 
-export default Overview;
+			<Button text='Edit' onClick={toggleForm} className='edit' />
+
+			{isFormVisible && (
+				<Form
+					fields={info}
+					onSubmit={handleSubmit}
+					onClickCancel={toggleForm}
+				/>
+			)}
+		</div>
+	);
+}
